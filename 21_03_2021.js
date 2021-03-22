a8bc7ff6a95e25b2e76abce0f6d6ea5b794c8c6c
Carlos Perez <cjperezvelez@gmail.com>
2021-03-21 21:32:48 -0500

    Completed 1 challenge

---
+++ 21_03_2021.js
@@ -0,0 +1,11 @@
+//Consider an array/list of sheep where some sheep may be missing from their place. We need a function that counts the number of sheep present in the array (true means present).
+
+function countSheeps(arrayOfSheep) {
+  let result = 0
+  for(let i = 0; i<arrayOfSheep.length; i++){
+    if(arrayOfSheep[i]==true){
+      result++
+    }
+  }
+  return result
+}
